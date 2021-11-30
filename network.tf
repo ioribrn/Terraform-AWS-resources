cfa89da30f7fa63387d92bb16316aeb3d9f639b6
nobo_devops <devops@nobo.life>
2021-11-30 22:47:19 +0100

    add vpc and network

--- network.tf
+++ network.tf
@@ -8,3 +8,24 @@ resource "aws_internet_gateway" "gw" {
     Name = "main-prod"
   }
 }
+
+
+#route table
+resource "aws_route_table" "routegw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "routegw"
+  }
+}
+
+#create association between route table and public subnet
+
+resource "aws_route_table_association" "pvsubnet" {
+  subnet_id      = aws_subnet.prod-subnet-public.id
+  route_table_id = aws_route_table.routegw.id
+}
